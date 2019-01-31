8306f2fdfddc86a124a495e29e135b67c71c1fd1
madhan <madhan.chiluka@gmail.com>
2019-01-31 14:06:46 +0530

    fqc-signin added

---
+++ services/fcq-constants.constant.js
@@ -0,0 +1,17 @@
+'use strict';
+angular.module('fqcApp').constant('FQC_PROPERTIES', {
+  auth: {
+    loginSuccess: 'auth-login-success',
+    loginFailed: 'auth-login-failed',
+    logoutSuccess: 'auth-logout-success',
+    notAuthenticated: 'auth-not-authenticated'
+  },
+  host: {
+   // for olms services
+    prod: 'http://192.168.0.37:9094/olms/rest/',
+    // for fqc services
+    url: 'http://192.168.0.31:8088/api/',
+    url1: 'http://192.168.0.31:8088/'
+
+  }
+})
